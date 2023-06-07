d8c3ac3bc49a088e89531fe49a818c0aaaac9865
Greg Tambornino <greg.tambornino@concordusa.com>
2023-06-07 17:04:33 -0500

    refactor custom widgets to separate files

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'card_child.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -27,21 +29,15 @@ class _InputPageState extends State<InputPage> {
                   ReusableCard(
                     color: activeCardColor,
                     cardChild: CardChild(
-                      cardIcon: Icon(
-                        FontAwesomeIcons.mars,
-                        size: 80,
-                      ),
-                      cardText: 'MALE',
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
                     ),
                   ),
                   ReusableCard(
                     color: activeCardColor,
                     cardChild: CardChild(
-                      cardIcon: Icon(
-                        FontAwesomeIcons.venus,
-                        size: 80,
-                      ),
-                      cardText: 'FEMALE',
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
                     ),
                   ),
                 ],
@@ -64,55 +60,5 @@ class _InputPageState extends State<InputPage> {
             )
           ],
         ));
-  }
-}
-
-class CardChild extends StatelessWidget {
-  const CardChild({required this.cardIcon, required this.cardText});
-
-  final Icon cardIcon;
-  final String cardText;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        cardIcon,
-        SizedBox(
-          height: 15,
-        ),
-        Text(
-          cardText,
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF8D8E98),
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.color, this.cardChild});
-
-  final Color color;
-  final Widget? cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Container(
-        child: cardChild,
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: color,
-        ),
-      ),
-    );
   }
 }
---
+++ lib/reusable_card.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.color, this.cardChild});
+
+  final Color color;
+  final Widget? cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color,
+        ),
+      ),
+    );
+  }
+}
