08893e41fe75d1e8c812c09dfea1f029f706989c
Greg Tambornino <greg.tambornino@concordusa.com>
2023-06-07 16:53:42 -0500

    refactor card icon widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -23,8 +24,26 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  ReusableCard(color: activeCardColor),
-                  ReusableCard(color: activeCardColor),
+                  ReusableCard(
+                    color: activeCardColor,
+                    cardChild: CardChild(
+                      cardIcon: Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80,
+                      ),
+                      cardText: 'MALE',
+                    ),
+                  ),
+                  ReusableCard(
+                    color: activeCardColor,
+                    cardChild: CardChild(
+                      cardIcon: Icon(
+                        FontAwesomeIcons.venus,
+                        size: 80,
+                      ),
+                      cardText: 'FEMALE',
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -48,16 +67,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class CardChild extends StatelessWidget {
+  const CardChild({required this.cardIcon, required this.cardText});
+
+  final Icon cardIcon;
+  final String cardText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        cardIcon,
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          cardText,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.color});
+  ReusableCard({required this.color, this.cardChild});
 
   final Color color;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 1,
       child: Container(
+        child: cardChild,
         margin: EdgeInsets.all(15),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
