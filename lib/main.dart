573e087d08c3184b7f20b268d544a6f9c020ff49
Greg Tambornino <greg.tambornino@concordusa.com>
2023-06-07 14:17:56 -0500

    alternate colors for theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,18 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.from(
-        // primaryColor: Color(0xFF0A0E21),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color(0xFF0A0E21),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          // seedColor: Color(0xFF0A0E21),
           primary: Color(0xFF0A0E21),
-          background: Color(0xFF0A0E21),
-          onBackground: Colors.white,
-          onPrimary: Colors.white,
+          secondary: Colors.purple,
+          // background: Color(0xFF0A0E21),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            color: Color(0xFFFFFFFF),
+          ),
         ),
         // appBarTheme: AppBarTheme(color: Colors.red),
       ),
