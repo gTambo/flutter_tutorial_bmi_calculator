44123bd212b07589b22b08f280a8d99a9b537d91
Greg Tambornino <greg.tambornino@concordusa.com>
2023-06-07 14:27:38 -0500

    theme from the default dark with customization

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           // seedColor: Color(0xFF0A0E21),
           primary: Color(0xFF0A0E21),
-          secondary: Colors.purple,
+          secondary: Colors.greenAccent,
+          onSecondary: Color(0xFF0A0E21),
           // background: Color(0xFF0A0E21),
         ),
         textTheme: const TextTheme(
